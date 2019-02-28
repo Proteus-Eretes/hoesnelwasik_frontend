6a2567fd513e4640978c8848b765743d8a6f1ecf
Douwe Koopmans <falling.dutchman@gmail.com>
2019-02-28 16:04:08 +0100

    add club overview
    data used is static and not retrieved from the API

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <div id="app">
-    <FinishTime time=500.15 bonusSeconds="15"></FinishTime>
-  </div>
+    <div id="app">
+        <FinishTime time="500.15" bonusSeconds="15"></FinishTime>
+        <ClubsOverview :clubs="getClubs()"></ClubsOverview>
+    </div>
 </template>
 
 <script>
-    // @ is an alias to /src
-    import FinishTime from "@/components/Time/FinishTime.vue";
-    import {Regatta} from "@/sercives/Regatta";
+// @ is an alias to /src
+import FinishTime from '@/components/Time/FinishTime.vue';
+import ClubsOverview from '@/components/Clubs/ClubsOverview.vue';
+import { Regatta } from '@/sercives/Regatta';
 
-
-
-    export default {
-        name: "home",
-        components: {
-            FinishTime
-        },
-        async mounted() {
-            const regatta = new Regatta('https://beta.hoesnelwasik.nl/api', 'ww', 0);
-            console.log(await regatta.getClubs());
-            regatta.setMatch('wwk', 0);
-            console.log(await regatta.getClubs());
+export default {
+    name: 'home',
+    components: {
+        FinishTime,
+        ClubsOverview
+    },
+    methods: {
+        getClubs() {
+            return [
+                { clubshort: 'AEG', clubname: 'Aegir' },
+                { clubshort: 'AMS', clubname: 'Amstel' }
+            ];
         }
-    };
+    },
+    async mounted() {
+        const regatta = new Regatta(
+            'https://beta.hoesnelwasik.nl/api',
+            'ww',
+            0
+        );
+        console.log(await regatta.getClubs());
+        regatta.setMatch('wwk', 0);
+        console.log(await regatta.getClubs());
+    }
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
---
+++ src/components/Clubs/ClubsOverview.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="clubs">
+        <b-table hover :items="prepareItems(clubs)" :fields="fields">
+            <template slot="oarImage" slot-scope="item">
+                <div class="oar">
+                    <b-img :src="item.item.blad" fluid alt="verenigings blad" />
+                </div>
+            </template>
+        </b-table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ClubsOverview',
+    props: {
+        clubs: Array
+    },
+    data() {
+        return {
+            fields: {
+                blad: {
+                    key: 'oarImage',
+                    label: 'blad'
+                },
+                clubshort: {
+                    label: 'code'
+                },
+                clubname: {
+                    label: 'vereniging'
+                }
+            }
+        };
+    },
+    methods: {
+        prepareItems(clubs) {
+            let res = clubs;
+
+            res.forEach(function(club) {
+                club.blad =
+                    'https://images.poweredbyiris.nl/0/30/cluboars/' +
+                    club.clubshort.toLowerCase() +
+                    '.png';
+            });
+
+            return res;
+        }
+    }
+};
+</script>
+
+<style scoped></style>
