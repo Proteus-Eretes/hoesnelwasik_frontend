190913649ace137604e62ce38eb31de7d7b93a53
Unknown <I.hoogenboom@student.tudelft.nl>
2019-03-01 14:38:46 +0100

    Start of teaminfo popup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <team-popup>
 
+    </team-popup>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import FinishTime from "@/components/Time/FinishTime.vue";
+import TeamPopup from "../components/Team/TeamPopup";
 
 export default {
   name: "home",
   components: {
+    TeamPopup,
     FinishTime
   }
 };
