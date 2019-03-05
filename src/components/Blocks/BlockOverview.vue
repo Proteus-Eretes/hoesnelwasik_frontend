27427092eeb21b5c5deb166a5fcf440098ea8c9f
Rens Hamburger <renshamburger@gmail.com>
2019-03-05 22:36:02 +0100

    Move block overview into card

--- src/components/Blocks/BlockOverview.vue
+++ src/components/Blocks/BlockOverview.vue
@@ -2,48 +2,19 @@
     <div class="container mt-3">
         <b-card-group columns style="column-count: 2">
             <div v-for="block in blocks" :key="block[0].blockid">
-                <b-card
-                    header-tag="header"
-                    header-bg-variant="dark"
-                    header-text-variant="white"
-                    no-body
-                >
-                    <h2 slot="header">{{getBlockTitle(block[0].blocknumber, block[0].starttime)}}</h2>
-                    <b-table striped head-variant="dark" :items="block" :fields="fields">
-                    </b-table>
-                </b-card>
+                <block-card :block="block"> </block-card>
             </div>
         </b-card-group>
     </div>
 </template>
 
 <script>
-
+import BlockCard from "./BlockCard";
 export default {
     name: 'BlockOverview',
+    components: { BlockCard },
     props: {
         blocks: Array
-    },
-    data() {
-        return {
-            fields: [
-                {key: 'fieldnameshort', label: 'Veld'},
-                {key: 'numberofteams', label: 'Aantal ploegen'},
-                {key: 'starttime', label: 'Starttijd'},
-            ],
-        }
-    },
-    methods: {
-        removeSeconds(starttime) {
-            return starttime.slice(0, starttime.lastIndexOf(':'));
-        },
-        getBlockTitle(blocknumber, starttime) {
-            return 'Block ' + blocknumber + ':  ' + this.removeSeconds(starttime);
-        }
     }
 };
 </script>
-
-<style scoped>
-
-</style>
