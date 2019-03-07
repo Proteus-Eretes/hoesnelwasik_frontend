3462ccbe01f2a3b042d3315d3d9309bdeef11446
Rens Hamburger <renshamburger@gmail.com>
2019-03-07 14:39:33 +0100

    Added results view for search and clubs

--- src/components/Time/FinalTime.vue
+++ src/components/Time/FinalTime.vue
@@ -15,7 +15,7 @@ export default {
     name: 'FinalTime',
     components: {FinishTime},
     props: {
-        time: String,
+        time: Number,
         bonusSeconds: String,
         status: String
     },
