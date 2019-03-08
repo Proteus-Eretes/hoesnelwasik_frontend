83f4a71ab1fc25194a62bd644e919e40d3154ea7
Rens Hamburger <renshamburger@gmail.com>
2019-03-08 14:09:38 +0100

    Added simple nav bar

---
+++ src/components/Navigation/ViewNavigationBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-tabs card class="full-width">
+        <b-tab
+            no-body
+            title-link-class="link-unstyled"
+            title-item-class="text-uppercase"
+            title="Veld"
+            v-on:click="navMatch('#velden')"
+        >
+        </b-tab>
+        <b-tab
+            no-body
+            title-link-class="link-unstyled"
+            title-item-class="text-uppercase"
+            title="Blok"
+            v-on:click="navMatch('#blocks')"
+        >
+        </b-tab>
+        <b-tab
+            no-body
+            title-link-class="link-unstyled"
+            title-item-class="text-uppercase"
+            title="Vereniging"
+            v-on:click="navMatch('#clubs')"
+        >
+        </b-tab>
+    </b-tabs>
+</template>
+
+<script>
+export default {
+    name: 'ViewNavigationBar',
+    props: {
+        type: String
+    },
+    methods: {
+        navMatch(hash) {
+            this.$router.push({
+                path: `/iframe/${this.$router.currentRoute.params.match}/${
+                    this.$router.currentRoute.params.year
+                }/${this.type}${hash}`
+            });
+        }
+    }
+};
+</script>
+
+<style scoped></style>
