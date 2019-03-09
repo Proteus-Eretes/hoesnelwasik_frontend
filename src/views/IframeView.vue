ebd81e73ed37904faeab5cd6e666b3549218e54c
Rens Hamburger <renshamburger@gmail.com>
2019-03-09 17:38:49 +0100

    First test + travis file

--- src/views/IframeView.vue
+++ src/views/IframeView.vue
@@ -36,6 +36,7 @@ export default {
     },
     methods: {
         init: async function() {
+            console.log('a');
             this.clubs = await this.regattaService.getClubs();
             this.blocks = await this.regattaService.getBlocks();
             this.events = await this.regattaService.getFields();
