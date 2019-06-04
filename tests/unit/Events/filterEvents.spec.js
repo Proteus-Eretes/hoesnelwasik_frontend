2c0b2181da95c2be8dc74bc3867f587aebff6fe1
Rens Hamburger <renshamburger@gmail.com>
2019-06-04 23:45:33 +0200

    Minor code clean up

--- tests/unit/Events/filterEvents.spec.js
+++ tests/unit/Events/filterEvents.spec.js
@@ -1,110 +1,108 @@
 import filterEvents from '../../../src/components/Events/filterEvents';
 
-const events = fieldnameshort => {
-    return [
-        {
-            id: 1,
-            isman: true,
-            islight: true,
-            fieldnameshort
-        },
-        {
-            id: 2,
-            isman: true,
-            islight: false,
-            fieldnameshort
-        },
-        {
-            id: 3,
-            isman: false,
-            islight: true,
-            fieldnameshort
-        },
-        {
-            id: 4,
-            isman: false,
-            islight: false,
-            fieldnameshort
-        },
-        {
-            id: 5,
-            isman: 1,
-            islight: 1,
-            fieldnameshort
-        },
-        {
-            id: 6,
-            isman: 1,
-            islight: 0,
-            fieldnameshort
-        },
-        {
-            id: 7,
-            isman: 0,
-            islight: 1,
-            fieldnameshort
-        },
-        {
-            id: 8,
-            isman: 0,
-            islight: 0,
-            fieldnameshort
-        },
-        {
-            id: 9,
-            isman: '1',
-            islight: '1',
-            fieldnameshort
-        },
-        {
-            id: 10,
-            isman: '1',
-            islight: '0',
-            fieldnameshort
-        },
-        {
-            id: 11,
-            isman: '0',
-            islight: '1',
-            fieldnameshort
-        },
-        {
-            id: 12,
-            isman: '0',
-            islight: '0',
-            fieldnameshort
-        },
-        {
-            id: 13,
-            isman: 1,
-            islight: null,
-            fieldnameshort
-        },
-        {
-            id: 14,
-            isman: null,
-            islight: 1,
-            fieldnameshort
-        },
-        {
-            id: 15,
-            isman: 0,
-            islight: null,
-            fieldnameshort
-        },
-        {
-            id: 16,
-            isman: null,
-            islight: 0,
-            fieldnameshort
-        }
-    ];
-};
+const events = fieldnameshort => [
+    {
+        id: 1,
+        isman: true,
+        islight: true,
+        fieldnameshort
+    },
+    {
+        id: 2,
+        isman: true,
+        islight: false,
+        fieldnameshort
+    },
+    {
+        id: 3,
+        isman: false,
+        islight: true,
+        fieldnameshort
+    },
+    {
+        id: 4,
+        isman: false,
+        islight: false,
+        fieldnameshort
+    },
+    {
+        id: 5,
+        isman: 1,
+        islight: 1,
+        fieldnameshort
+    },
+    {
+        id: 6,
+        isman: 1,
+        islight: 0,
+        fieldnameshort
+    },
+    {
+        id: 7,
+        isman: 0,
+        islight: 1,
+        fieldnameshort
+    },
+    {
+        id: 8,
+        isman: 0,
+        islight: 0,
+        fieldnameshort
+    },
+    {
+        id: 9,
+        isman: '1',
+        islight: '1',
+        fieldnameshort
+    },
+    {
+        id: 10,
+        isman: '1',
+        islight: '0',
+        fieldnameshort
+    },
+    {
+        id: 11,
+        isman: '0',
+        islight: '1',
+        fieldnameshort
+    },
+    {
+        id: 12,
+        isman: '0',
+        islight: '0',
+        fieldnameshort
+    },
+    {
+        id: 13,
+        isman: 1,
+        islight: null,
+        fieldnameshort
+    },
+    {
+        id: 14,
+        isman: null,
+        islight: 1,
+        fieldnameshort
+    },
+    {
+        id: 15,
+        isman: 0,
+        islight: null,
+        fieldnameshort
+    },
+    {
+        id: 16,
+        isman: null,
+        islight: 0,
+        fieldnameshort
+    }
+];
 
 describe('Events are filtered correctly', () => {
     it('Filter on man - isLight', () => {
         const fieldnameshort = 'X 1x';
-        expect(filterEvents(events('X 1x'), true, true)).toEqual([
+        expect(filterEvents(events(fieldnameshort), true, true)).toEqual([
             {
                 id: 1,
                 isman: true,
@@ -128,7 +126,7 @@ describe('Events are filtered correctly', () => {
 
     it('Filter on man', () => {
         const fieldnameshort = 'X 1x';
-        expect(filterEvents(events('X 1x'), true, false)).toEqual([
+        expect(filterEvents(events(fieldnameshort), true, false)).toEqual([
             {
                 id: 2,
                 isman: true,
@@ -158,7 +156,7 @@ describe('Events are filtered correctly', () => {
 
     it('Filter on woman - isLight', () => {
         const fieldnameshort = 'X 1x';
-        expect(filterEvents(events('X 1x'), false, true)).toEqual([
+        expect(filterEvents(events(fieldnameshort), false, true)).toEqual([
             {
                 id: 3,
                 isman: false,
@@ -188,7 +186,7 @@ describe('Events are filtered correctly', () => {
 
     it('Filter on woman', () => {
         const fieldnameshort = 'X 1x';
-        expect(filterEvents(events('X 1x'), false, false)).toEqual([
+        expect(filterEvents(events(fieldnameshort), false, false)).toEqual([
             {
                 id: 4,
                 isman: false,
