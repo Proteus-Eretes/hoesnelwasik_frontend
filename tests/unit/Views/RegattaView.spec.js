34435c1907152a232bb6196c61afdcf1e679d4f0
Rens Hamburger <renshamburger@gmail.com>
2020-01-30 19:25:38 +0100

    Fix failing test

--- tests/unit/Views/RegattaView.spec.js
+++ tests/unit/Views/RegattaView.spec.js
@@ -1,10 +1,19 @@
-import { shallowMount } from '@vue/test-utils';
+import {createLocalVue, shallowMount} from '@vue/test-utils';
 import RegattaView from '../../../src/Views/RegattaView';
+import VueRouter from 'vue-router';
 
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+
+const router = new VueRouter();
+
+router.options.api = 'api';
 describe('RegattaView.vue', () => {
     it('Renders', () => {
         const initSpy = jest.fn();
         const wrapper = shallowMount(RegattaView, {
+            localVue,
+            router,
             methods: {
                 init: initSpy
             }
