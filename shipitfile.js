cc60e254bd9d5bfe0065ee1e0b7aba0b3d616a9e
Iwan Hoogenboom <I.hoogenboom@student.tudelft.nl>
2020-05-15 21:01:36 +0200

    Update shipitfile.js

--- shipitfile.js
+++ shipitfile.js
@@ -32,7 +32,8 @@ module.exports = shipit => {
                 'https://github.com/Proteus-Eretes/hoesnelwasik_frontend.git'
         },
         live: {
-            servers: 'dev@poweredbyiris.nl'
+//             servers: 'dev@poweredbyiris.nl'
+            servers: 'dev@64.225.77.148'
         }
     });
 };
