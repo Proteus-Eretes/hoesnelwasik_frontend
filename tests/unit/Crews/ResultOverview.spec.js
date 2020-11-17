3df0a6811f1029c58cbe802393cc4b6bf06da990
Iwan <I.hoogenboom@student.tudelft.nl>
2020-11-17 22:38:31 +0100

    Added a test to include all round

--- tests/unit/Crews/ResultOverview.spec.js
+++ tests/unit/Crews/ResultOverview.spec.js
@@ -12,7 +12,7 @@ const router = new VueRouter();
 localVue.use(BootstrapVue);
 
 describe('ResultOverview', () => {
-    it('Renders the title correctly and fields', () => {
+    it('Renders the title correctly and fields in a race with clocking locations', () => {
         const wrapper = mount(ResultOverview, {
             localVue,
             router,
@@ -110,5 +110,116 @@ describe('ResultOverview', () => {
         expect(wrapper.html()).toMatch('>03:30.0</td>');
         expect(wrapper.html()).toMatch('>04:00.1</td>');
         expect(wrapper.html()).toMatch('05:01.2');
+    });
+
+    it('Renders the title correctly and fields in a race with multiple rounds', () => {
+        const wrapper = mount(ResultOverview, {
+            localVue,
+            router,
+            propsData: {
+                crews: [
+                    {
+                        knrbid: '11e9-2fcd-3c1be544-b1c6-525400d1387d',
+                        teamname: 'Orca',
+                        fieldnameshort: 'DG 1x',
+                        fieldnameshortsub: 'DG 1x',
+                        clubname: 'Orca',
+                        clubnameshort: 'ORC',
+                        rower8: 'Piet de Test',
+                        coachnames: 'Coach van piet',
+                        times: [
+                            {
+                                knrbid: '11e9-2fcd-3c1be544-b1c6-525400d1387d',
+                                status: '0',
+                                rank: 1,
+                                backnumber: '149',
+                                bonussecond: '0',
+                                comments: '',
+                                disqualified: 0,
+                                excluded: 0,
+                                round: '11e9-17ee-922eab8e-bcfe-525400d1387f',
+                                alteredTime: 1284.945,
+                                times: [
+                                    {
+                                        time: 60.2,
+                                        distance: '5000',
+                                        name: 'Ronde 1',
+                                        type: 'Finish',
+                                        equal_time_correction: '0',
+                                        alteredTime: 1284.945
+                                    }
+                                ],
+                                subRank: 1
+                            },
+                            {
+                                knrbid: '11e9-2fcd-3c1be544-b1c6-525400d1387s',
+                                status: '0',
+                                rank: 1,
+                                backnumber: '149',
+                                bonussecond: '0',
+                                comments: '',
+                                disqualified: 0,
+                                excluded: 0,
+                                round: '11e9-17ee-922eab8e-bcfe-525400d1387a',
+                                alteredTime: 1284.945,
+                                times: [
+                                    {
+                                        time: 120,
+                                        distance: '3000',
+                                        name: 'Ronde 2',
+                                        type: 'Finish',
+                                        equal_time_correction: '0',
+                                        alteredTime: 1285.945
+                                    }
+                                ],
+                                subRank: 1
+                            },
+                            {
+                                totaltime: 1231,
+                                splash1: 0,
+                                splash2: 0,
+                                splash3: 0,
+                                splash4: 0,
+                                bonussecond: 0,
+                                status: 0,
+                                equal_time_correction: 0,
+                                times: [
+                                    {
+                                        time: 301.2,
+                                        distance: 0,
+                                        name: 'Total',
+                                        type: 'Calculation',
+                                        equal_time_correction: 0,
+                                        alteredTime: 1284.945
+                                    }
+                                ],
+                                subRank: 1
+                            }
+                        ]
+                    }
+                ]
+            }
+        });
+        expect(wrapper.html()).toMatch('Blad');
+        expect(wrapper.html()).toMatch('Pos');
+        expect(wrapper.html()).toMatch('Veld');
+        expect(wrapper.html()).toMatch('Rug#');
+        expect(wrapper.html()).toMatch('Ploeg');
+        expect(wrapper.html()).toMatch('Slag');
+        expect(wrapper.html()).toMatch('Ronde 1');
+        expect(wrapper.html()).toMatch('Ronde 2');
+        expect(wrapper.html()).toMatch('Finish tijd');
+
+
+        expect(wrapper.html()).toMatch('>1</td>');
+        expect(wrapper.html()).toMatch('>DG 1x</td>');
+        expect(wrapper.html()).toMatch('>149</td>');
+        expect(wrapper.html()).toMatch('>Orca</td>');
+        expect(wrapper.html()).toMatch('>Piet de Test</td>');
+        expect(wrapper.html()).toMatch('>01:00.2</td>');
+        expect(wrapper.html()).toMatch('>02:00.0</td>');
+        expect(wrapper.html()).toMatch('05:01.2');
+
+
     });
 });
